a53768a0af7d3300cdd1860cbadb355e0bffa934
Ayaan Farrukh <ayaanfarrukhraza@gmail.com>
2025-04-15 17:39:31 +0500

    Added Golden Apple Feature

--- snake.js
+++ snake.js
@@ -14,6 +14,7 @@ let directions = {
     y: 0
 };
 
+let goldenFoodPosition;
 let foodPosition;
 let endBox = document.querySelector('.playagain');
 let againButton = document.querySelector('.btn-again');
@@ -22,23 +23,26 @@ let scoreMsg = document.querySelector('.score');
 let playButton = document.querySelector('.play-btn');
 let startBox = document.querySelector('.startbox');
 let x = 200;
+let play = 10000;
 let scoreCounter = 0;
 let highestScore = 0;
 const eatSound = new Audio("./audios/bite.mp3");
+let goldenFoodIndex;
+let goldenInterval;
 
 function snakeBodyDisplay() {
     let cells = document.querySelectorAll('.cell');
 
     cells.forEach((cell,i) => {
         foodIndex = foodPosition.y * 20 + foodPosition.x;
-        if(i !== foodIndex) {
+        if(goldenFoodPosition) {
+            goldenFoodIndex = goldenFoodPosition.y * 20 + goldenFoodPosition.x;
+        }
+
+        if(i !== foodIndex && i !== goldenFoodIndex) {
             cell.style.backgroundColor = "";
             cell.style.backgroundImage = "";
         }
-    });
-     //removing eye 
-     cells.forEach((cells) => {
-        cells.classList.remove('snake-head');
     });
 
     //rendering snake moving:
@@ -46,27 +50,24 @@ function snakeBodyDisplay() {
     let index = bodyPart.y * 20 + bodyPart.x; //rendering index
     cells[index].style.backgroundColor = "green";
 
-    if(i === 0 && directions.x === 1) { //rendering rounded corners of new heads of snake
-        cells[index].style.borderTopRightRadius = "10px";
-        cells[index].style.borderBottomRightRadius = "10px";
+     if(i === 0 && directions.x === 1) { //rendering rounded corners of new heads of snake
+         cells[index].style.borderTopRightRadius = "10px";
+         cells[index].style.borderBottomRightRadius = "10px";
     } else if(i === 0 && directions.x === -1) {
+         cells[index].style.borderTopLeftRadius = "10px";
+         cells[index].style.borderBottomLeftRadius = "10px";
+
+     } else if(i === 0 && directions.y === 1) {
+         cells[index].style.borderBottomRightRadius = "10px";
+         cells[index].style.borderBottomLeftRadius = "10px";
+
+     } else if(i === 0 && directions.y === -1) {
+         cells[index].style.borderTopRightRadius = "10px";
         cells[index].style.borderTopLeftRadius = "10px";
-        cells[index].style.borderBottomLeftRadius = "10px";
+     } else {
+         cells[index].style.borderRadius = "0px";
+     }
 
-    } else if(i === 0 && directions.y === 1) {
-        cells[index].style.borderBottomRightRadius = "10px";
-        cells[index].style.borderBottomLeftRadius = "10px";
-
-    } else if(i === 0 && directions.y === -1) {
-        cells[index].style.borderTopRightRadius = "10px";
-        cells[index].style.borderTopLeftRadius = "10px";
-    } else {
-        cells[index].style.borderRadius = "0px";
-    }
-
-    if(i === 0) {
-        cells[index].classList.add('snake-head'); //adding eye
-    };
     }) 
 
 };
@@ -85,6 +86,25 @@ function foodAppear() {
             let index = y * 20 + x; 
             let cells = document.querySelectorAll('.cell');
             cells[index].style.backgroundImage = "url('https://freesvg.org/img/simple-apple.png')";
+            cells[index].style.backgroundSize = "60%";
+            cells[index].style.backgroundRepeat = "no-repeat";
+            cells[index].style.backgroundPosition = "center";
+            break;
+        }
+    }
+}
+
+function GoldenfoodAppear() {
+    while(true) {
+        const x = Math.floor(Math.random()*20);
+        const y = Math.floor(Math.random()*20);
+        //check if food on snake
+        const isOnSnake = snakeBody.some(part => part.x === x && part.y === y);
+        if(!isOnSnake && x !== foodPosition.x && y !== foodPosition.y) {
+            goldenFoodPosition = {x,y};
+            let index = y * 20 + x; 
+            let cells = document.querySelectorAll('.cell');
+            cells[index].style.backgroundImage = "url('https://cdn-icons-png.flaticon.com/512/2298/2298998.png')";
             cells[index].style.backgroundSize = "60%";
             cells[index].style.backgroundRepeat = "no-repeat";
             cells[index].style.backgroundPosition = "center";
@@ -125,6 +145,8 @@ snakeBody.forEach((bodyPart) => {
         scoreMsg.innerHTML = `Your Score: ${scoreCounter} | Highest Score: ${highestScore}`;
         gameEndMsg();
         clearInterval(gameInterval); 
+        play = 30000;
+        clearInterval(goldenInterval);
         scoreCounter = 0;
         return;
     }
@@ -141,6 +163,8 @@ if (
     scoreMsg.innerHTML = `Your Score: ${scoreCounter} | Highest Score: ${highestScore}`;
     gameEndMsg();
     clearInterval(gameInterval);
+    play = 30000;
+    clearInterval(goldenInterval);
     scoreCounter = 0;
     return;
 }
@@ -165,6 +189,24 @@ if(newHead.x === foodPosition.x && newHead.y === foodPosition.y ) { //snake eat 
     snakeBody.unshift(newHead);
     snakeBody.pop();
 }
+
+if(goldenFoodPosition) { //added the golden feature
+    if(newHead.x === goldenFoodPosition.x && newHead.y === goldenFoodPosition.y ) {
+        eatSound.play();
+        setTimeout(() => {
+            eatSound.pause();
+            eatSound.currentTime = 0;
+        },1000)
+        snakeBody = [snakeBody[0]];
+           play = 60000;
+           clearInterval(goldenInterval);
+           setInterval(() => {
+           GoldenfoodAppear();
+           },play);
+           goldenFoodPosition = {x: null, y: null};
+        }
+}
+
 
 
 snakeBodyDisplay();
@@ -219,11 +261,21 @@ againButton.addEventListener('click', () => {
     x = 200;
     snakeBodyDisplay();
     foodAppear();
+    GoldenfoodAppear();
     gameInterval = setInterval(moveSnake,x);
     endBox.classList.remove('js-endbox');
+    play = 30000;
+    clearInterval(goldenInterval);
+    goldenInterval = setInterval(() => {
+        GoldenfoodAppear();
+        },play)
 });
 
 playButton.addEventListener('click',() => {
 startBox.classList.add('js-startbox');
 gameInterval = setInterval(moveSnake,x);
 });
+
+goldenInterval = setInterval(() => {
+GoldenfoodAppear();
+},play)
